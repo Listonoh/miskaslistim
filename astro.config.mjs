05cc64a05ba95964e619c15a810eaaca6365302d
Jan Urban <41571832+Listonoh@users.noreply.github.com>
2026-03-28 10:50:23 +0100

    Fix base path by adding trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://listonoh.github.io',
-  base: '/miskaslistim',
+  base: '/miskaslistim/',
   output: 'static',
 });
