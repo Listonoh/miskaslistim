03215ead2c2eb5136341e02d39a1bf84e3daacb5
Jan Urban <41571832+Listonoh@users.noreply.github.com>
2026-05-11 17:47:30 +0000

    chore: update package.json to add ESLint and related dependencies
    refactor: clean up Footer.astro by removing unused styles
    
    refactor: simplify Header.astro markup and styles
    
    refactor: remove unused styles from about.astro
    
    refactor: adjust index.astro structure and styles for better layout
    
    style: clean up global.css by removing unnecessary max-width for paragraphs

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import astro from "eslint-plugin-astro";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
+
+const unusedVarsRule = [
+  "error",
+  {
+    argsIgnorePattern: "^_",
+    varsIgnorePattern: "^_"
+  }
+];
+
+export default [
+  {
+    ignores: ["dist/**", ".astro/**"]
+  },
+  ...astro.configs.recommended,
+  {
+    files: [
+      "**/*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}",
+      "**/*.astro/*.js",
+      "**/*.astro/*.ts"
+    ],
+    plugins: {
+      "@typescript-eslint": tsEslintPlugin
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": unusedVarsRule
+    }
+  },
+  {
+    files: ["**/*.astro"],
+    rules: {
+      "astro/no-unused-css-selector": "error"
+    }
+  }
+];
